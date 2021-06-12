6a5012f9803ea3a4b548da79cf9095044cbbab3e
balu-pinisetty <balupinisetty@gmail.com>
2021-06-12 10:18:17 +0530

    Added intro message

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World\nJAVA welcomes you");
+		
+		System.out.println("Lets make an Intro");
 }
 }
